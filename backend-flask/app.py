d87342f48c9113d61c2ace314527a2713bd731ef
Kristoffer Laiz <kristoffer.laiz@outlook.com>
2023-02-28 02:15:05 +0800

    week 2: instrument xray

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -22,6 +22,10 @@ from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExport
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter, SimpleSpanProcessor
 
+# X-RAY --------------
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
 
 # HoneyComb --------------
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -37,6 +41,12 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+
+# X-RAY --------------
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask-xray', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
 
 # HoneyComb --------------
 # Initialize automatic instrumentation with Flask
