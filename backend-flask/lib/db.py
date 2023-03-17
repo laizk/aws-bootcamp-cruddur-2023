03f36654dc4d9795ed881f150b2236000030705d
Kristoffer Laiz <kristoffer.laiz@outlook.com>
2023-03-18 01:29:17 +0800

    week 4: create activity refinements

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -76,6 +76,7 @@ class Db:
   # when we want to return a json object
   def query_object_json(self,sql, params={}):
     self.print_sql('json', sql)
+    self.print_params(params)
     wrapped_sql = self.query_wrap_object(sql)
     with self.pool.connection() as conn:
       with conn.cursor() as cur:
